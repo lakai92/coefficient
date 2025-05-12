6254c62d3d75f1ed28e298fefa5413f1f3612b3c
lakai92 <57639913+lakai92@users.noreply.github.com>
2025-05-12 20:49:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-document.title = "whynot";
+document.title = "whynoterror";
+
+const errorOverlay = document.createElement('div');
+errorOverlay.style.position = 'fixed';
+errorOverlay.style.top = '0';
+errorOverlay.style.left = '0';
+errorOverlay.style.width = '100vw';
+errorOverlay.style.height = '100vh';
+errorOverlay.style.backgroundColor = 'white';
+errorOverlay.style.display = 'flex';
+errorOverlay.style.alignItems = 'center';
+errorOverlay.style.justifyContent = 'center';
+errorOverlay.style.zIndex = '9999'; // поверх всего
+
+const errorText = document.createElement('div');
+errorText.textContent = 'error';
+errorText.style.fontSize = '2rem';
+errorText.style.color = 'black';
+
+errorOverlay.appendChild(errorText);
+document.body.prepend(errorOverlay);
+
 
 window.scriptLoaded = true;
